f3b858dbf86a5d3db36eddb27ecd7ab790ef6c7f
Santiagotamayoz <tamayozuluagas@gmail.com>
2023-12-04 15:35:02 -0500

    crear proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entradas del problema
+        String nombreUsuario="Miguel Angel Gonzalez Higuita";
+        short edadUsuario=17;
+        String telefonoUsuario="3006770560";
+        String documentoIdentidad="1033488494";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario="Cll 50# 80-20";
+        double costoInscripcion=150000.0;
+        float tasaInteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+        //PROCESO
+
+        //1. Asignar un valor especifico a una variable
+        //desde el TECLADO (recibir datos)
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola Soy una IA, Cual es tu nombre?");
+        leerDato.nextLine();
+
+
+
+    }
+}
